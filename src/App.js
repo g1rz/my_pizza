354e8b10aae4238100062954bc7fa34f38ce84c0
Sergey Peshkov <zloi.g1rz@gmail.com>
2020-10-08 01:35:41 +0800

    Начал верстку товаров

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.sass';
 import Header from './components/Header/Header';
 import Categories from './components/Categories/Categories';
+import ProductBlock from './components/ProductBlock/ProductBlock';
 
 function App() {
     const [activeCategory, setActiveCategory] = React.useState('all');
@@ -14,11 +15,9 @@ function App() {
         <div className="App">
             <Header />
             <div className="container">
-                <div className="row row--space-b">
-                    <Categories
-                        activeCategory={activeCategory}
-                        onSelectCategory={onSelectCategory}
-                    />
+                <Categories activeCategory={activeCategory} onSelectCategory={onSelectCategory} />
+                <div className="pizza-list row row--wrap">
+                    <ProductBlock />
                 </div>
             </div>
         </div>
---
+++ src/components/ProductBlock/ProductBlock.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Button from '../Button';
+
+import './ProductBlock.sass';
+
+const ProductBlock = () => {
+    return (
+        <div className="product">
+            <div className="product__img-wrap">
+                <img
+                    src="https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/1e1a6e80-b3ba-4a44-b6b9-beae5b1fbf27.jpg"
+                    alt=""
+                    className="product__img"
+                />
+            </div>
+            <p className="product__title">Крэйзи пепперони</p>
+            <p className="product__desc">
+                Ветчина, шампиньоны, увеличенная порция моцареллы, томатный соус
+            </p>
+            <div className="product__row row row--space-b">
+                <div className="product__price-start">от 345 ₽</div>
+                <Button className="product__btn">Добавить</Button>
+            </div>
+        </div>
+    );
+};
+
+export default ProductBlock;
