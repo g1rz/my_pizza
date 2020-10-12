8fecc78a4def0b25ac969b195729837023d1ea65
Sergey Peshkov <zloi.g1rz@gmail.com>
2020-10-13 00:24:07 +0800

    Вывод пицц из базы

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.sass';
+import axios from 'axios';
 import Header from './components/Header/Header';
 import Categories from './components/Categories/Categories';
 import ProductBlock from './components/ProductBlock/ProductBlock';
@@ -8,6 +9,12 @@ import ProductPopup from './components/ProductPopup/ProductPopup';
 function App() {
     const [activeCategory, setActiveCategory] = React.useState('all');
     const [isShowPopup, setIsShowPopup] = React.useState(false);
+
+    const [items, setItems] = React.useState([]);
+
+    React.useEffect(() => {
+        axios.get('http://localhost:3001/pizzas').then(({ data }) => setItems(data));
+    }, []);
 
     const onSelectCategory = (category) => {
         setActiveCategory(category);
@@ -19,12 +26,7 @@ function App() {
             <div className="container">
                 <Categories activeCategory={activeCategory} onSelectCategory={onSelectCategory} />
                 <div className="pizza-list row row--wrap ">
-                    <ProductBlock />
-                    <ProductBlock />
-                    <ProductBlock />
-                    <ProductBlock />
-                    <ProductBlock />
-                    <ProductBlock />
+                    {items && items.map((item) => <ProductBlock key={item.id} {...item} />)}
                 </div>
             </div>
             {isShowPopup && <ProductPopup />}
--- src/components/ProductBlock/ProductBlock.jsx
+++ src/components/ProductBlock/ProductBlock.jsx
@@ -3,23 +3,19 @@ import Button from '../Button';
 
 import './ProductBlock.sass';
 
-const ProductBlock = ({ id, imageUrl, name, content, price }) => {
+const ProductBlock = ({ id, imageUrl, name, contain, products }) => {
+    products.sort((a, b) => (a.price > b.price ? 1 : -1));
+
     return (
         <div className="product-wrap">
             <div className="product">
                 <div className="product__img-wrap">
-                    <img
-                        src="https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/1e1a6e80-b3ba-4a44-b6b9-beae5b1fbf27.jpg"
-                        alt=""
-                        className="product__img"
-                    />
+                    <img src={imageUrl} alt={name} className="product__img" />
                 </div>
-                <p className="product__title">Крэйзи пепперони</p>
-                <p className="product__desc">
-                    Ветчина, шампиньоны, увеличенная порция моцареллы, томатный соус
-                </p>
+                <p className="product__title">{name}</p>
+                <p className="product__desc">{contain}</p>
                 <div className="product__row row row--space-b row--align-c">
-                    <div className="product__price-start">от 345 ₽</div>
+                    <div className="product__price-start">от {products[0].price} ₽</div>
                     <Button className="product__btn">Выбрать</Button>
                 </div>
             </div>
